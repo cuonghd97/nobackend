ac08721aa618547773f21d4f5860db8a4a9f9772
cuong <cuong.hoangduc@cxview.ai>
2022-09-05 09:57:19 +0000

    Init project

---
+++ src/main/java/com/grape/nobackend/NobackendApplication.java
@@ -0,0 +1,13 @@
+package com.grape.nobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grape/nobackend/NobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grape.nobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
